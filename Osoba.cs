d2b38ff0279a28cfc695642371d1d4bd0746ce87
PMBytner <pawel.m.bytner@gmail.com>
2022-05-22 08:46:42 +0200

    początek implementacji Osoba, z metodą OnPropertyChanged

---
+++ Osoba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_ININ3_PR2_z3
+{
+    class Osoba : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        readonly static Dictionary<string, string[]> powiązaneWłaściwości
+            = new Dictionary<string, string[]>()
+            {
+                ["Imię"] = new string[] {"ImięNazwisko"}
+            };
+        void OnPropertyChanged([CallerMemberName] string właściwość = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(właściwość));
+            foreach (string powiązanaWłaściwość in powiązaneWłaściwości[właściwość])
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(powiązanaWłaściwość));
+
+        }
+
+    }
+}
